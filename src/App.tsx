7948aa4f48ea8e10f42de9e1544b915dcbf82509
someoddperson <pawel.a.rybak@gmail.com>
2019-05-23 21:28:26 +0200

    Change router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { StyleSheet, css } from 'aphrodite';
 
 import './assets/Lato/latofonts.css';
